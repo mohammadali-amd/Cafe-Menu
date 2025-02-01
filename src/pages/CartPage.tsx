05976179fb95ccf3adb8e009a795c0f9e5f8ed42
mohammadali <fitapp999@gmail.com>
2025-02-01 23:40:19 +0330

    update

--- src/pages/CartPage.tsx
+++ src/pages/CartPage.tsx
@@ -12,82 +12,83 @@ const CartPage = () => {
   } = useCart();
 
   return (
-    <div className="p-4">
-      <h1 className="text-2xl text-center font-bold mb-4 text-white">
+    <div>
+      <h1 className="text-2xl text-center font-bold mb-4 text-white bg-stone-800 p-4">
         لیست سفارشات
       </h1>
-
-      {cartItems.length === 0 ? (
-        <p className="text-white text-center">لیست سفارشات شما خالی است</p>
-      ) : (
-        <>
-          <div className="space-y-4">
-            {cartItems.map((cartItem) => (
-              <div
-                key={cartItem.item.title}
-                className="bg-stone-200 rounded-lg p-4"
-              >
-                <div className="flex justify-between items-center">
-                  <div className="flex gap-2 items-center">
-                    <img
-                      src={cartItem.item.image}
-                      alt={cartItem.item.title}
-                      className="w-16 h-16 rounded-lg object-cover"
-                      loading="lazy"
-                    />
-                    <div>
-                      <h3 className="text-lg font-semibold">
-                        {cartItem.item.title}
-                      </h3>
-                      <p className="text-rose-700">
-                        {PersianNumber(cartItem.item.price.toLocaleString())}{" "}
-                        تومان
-                      </p>
+      <div className="p-4">
+        {cartItems.length === 0 ? (
+          <p className="text-white text-center">لیست سفارشات شما خالی است</p>
+        ) : (
+          <>
+            <div className="space-y-4">
+              {cartItems.map((cartItem) => (
+                <div
+                  key={cartItem.item.title}
+                  className="bg-stone-200 rounded-lg p-4"
+                >
+                  <div className="flex justify-between items-center">
+                    <div className="flex gap-2 items-center">
+                      <img
+                        src={cartItem.item.image}
+                        alt={cartItem.item.title}
+                        className="w-16 h-16 rounded-lg object-cover"
+                        loading="lazy"
+                      />
+                      <div>
+                        <h3 className="text-lg font-semibold">
+                          {cartItem.item.title}
+                        </h3>
+                        <p className="text-rose-700">
+                          {PersianNumber(cartItem.item.price.toLocaleString())}{" "}
+                          تومان
+                        </p>
+                      </div>
+                    </div>
+                    <div className="flex items-center gap-3">
+                      <button
+                        onClick={() => decreaseQuantity(cartItem.item)}
+                        className="bg-rose-600 text-white px-3 py-1 rounded"
+                      >
+                        -
+                      </button>
+                      <span className="w-4 text-center text-lg">
+                        {PersianNumber(cartItem.quantity.toString())}
+                      </span>
+                      <button
+                        onClick={() => addToCart(cartItem.item)}
+                        className="bg-yellow-500 text-white px-3 py-1 rounded"
+                      >
+                        +
+                      </button>
                     </div>
                   </div>
-                  <div className="flex items-center gap-3">
-                    <button
-                      onClick={() => decreaseQuantity(cartItem.item)}
-                      className="bg-rose-600 text-white px-3 py-1 rounded"
-                    >
-                      -
-                    </button>
-                    <span className="w-4 text-center text-lg">
-                      {PersianNumber(cartItem.quantity.toString())}
-                    </span>
-                    <button
-                      onClick={() => addToCart(cartItem.item)}
-                      className="bg-yellow-500 text-white px-3 py-1 rounded"
-                    >
-                      +
-                    </button>
-                  </div>
                 </div>
+              ))}
+            </div>
+
+            <div className="mt-6 p-4 bg-stone-700 rounded-lg">
+              <div className="flex justify-between text-white">
+                <span>تعداد سفارشات :</span>
+                <span>{PersianNumber(totalItems.toString())}</span>
               </div>
-            ))}
-          </div>
-
-          <div className="mt-6 p-4 bg-stone-700 rounded-lg">
-            <div className="flex justify-between text-white">
-              <span>تعداد سفارشات :</span>
-              <span>{PersianNumber(totalItems.toString())}</span>
+              <div className="flex justify-between text-white mt-2">
+                <span>مجموع قیمت :</span>
+                <span>{PersianNumber(totalPrice.toLocaleString())} تومان</span>
+              </div>
             </div>
-            <div className="flex justify-between text-white mt-2">
-              <span>مجموع قیمت :</span>
-              <span>{PersianNumber(totalPrice.toLocaleString())} تومان</span>
-            </div>
-          </div>
 
-          <div className="mt-4 flex gap-2">
-            <button
-              onClick={clearCart}
-              className="flex-1 bg-rose-700 text-white py-2 rounded-lg"
-            >
-              حذف تمام سفارشات
-            </button>
-          </div>
-        </>
-      )}
+            <div className="mt-4 flex gap-2">
+              <button
+                onClick={clearCart}
+                className="flex-1 bg-rose-700 text-white py-2 rounded-lg"
+              >
+                حذف تمام سفارشات
+              </button>
+            </div>
+          </>
+        )}
+      </div>
     </div>
   );
 };
--- src/pages/InformationPage.tsx
+++ src/pages/InformationPage.tsx
@@ -1,27 +1,38 @@
 const InformationPage = () => {
   return (
-    <div className="p-4">
-      <h2 className="text-center text-white text-2xl mb-4">رستوران سنتی</h2>
-
-      <div className="flex flex-col bg-stone-200 rounded-lg px-4 py-2 divide-y divide-stone-700">
-        <div className="flex flex-col gap-2 py-2">
-          <h3 className="font-semibold text-lg">تلفن</h3>
-          <p>0912345678</p>
+    <div>
+      <h1 className="text-center font-bold text-white text-2xl mb-4 bg-stone-800 p-4">
+        سفره خانه سنتی کیان
+      </h1>
+      <div className="p-4">
+        <div className="flex flex-col bg-stone-200 rounded-lg px-4 py-2 divide-y divide-stone-700">
+          <div className="flex flex-col gap-2 py-2">
+            <h3 className="font-semibold text-lg">تلفن</h3>
+            <p>0912345678</p>
+          </div>
+          <div className="flex flex-col gap-2 py-2">
+            <h3 className="font-semibold text-lg">ساعت کاری</h3>
+            <p>از ساعت 8 صبح تا 12 شب</p>
+          </div>
+          <div className="flex flex-col gap-2 py-2">
+            <h3 className="font-semibold text-lg">آدرس</h3>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt eos
+              quaerat veritatis culpa blanditiis aperiam nihil, dolore
+              reiciendis quis sapiente omnis, ullam quasi sit distinctio dolor
+              voluptate adipisci ratione voluptatibus.
+            </p>
+            <div className="py-2">لوکیشن</div>
+          </div>
+          <div className="py-2">اینستاگرام</div>
+          <div className="py-2">
+            <p>
+              انجام تمامی سفارشات
+              <br />
+              رزرو میز برای مهمانی و جشن های شما
+            </p>
+          </div>
         </div>
-        <div className="flex flex-col gap-2 py-2">
-          <h3 className="font-semibold text-lg">ساعت کاری</h3>
-          <p>از ساعت 8 صبح تا 12 شب</p>
-        </div>
-        <div className="flex flex-col gap-2 py-2">
-          <h3 className="font-semibold text-lg">آدرس</h3>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt eos
-            quaerat veritatis culpa blanditiis aperiam nihil, dolore reiciendis
-            quis sapiente omnis, ullam quasi sit distinctio dolor voluptate
-            adipisci ratione voluptatibus.
-          </p>
-        </div>
-        <div className="py-2">لوکیشن</div>
       </div>
     </div>
   );
